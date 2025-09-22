8d0499bf4dc1a49eeb955a6f9f8c09e72311aeb8
ponlawat-w <ponlawat_w@outlook.co.th>
2025-09-22 13:56:46 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_forumpostratelimit';
-$plugin->release = '0.1.0';
-$plugin->version = 2025021700;
+$plugin->release = '0.2.0';
+$plugin->version = 2025092200;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
